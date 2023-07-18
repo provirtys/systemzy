dee68fe4dad60434ec034a0dab917023ac853c94
provirtys <provirtys@gmail.com>
2023-07-18 21:33:13 +0500

    add top-slider section

--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,51 @@
 // polyfills
-import '@babel/polyfill/dist/polyfill';
+import "@babel/polyfill/dist/polyfill";
 // styles
-import '../scss/style.scss';
+import "../scss/style.scss";
+import "swiper/swiper.min.css";
+// import "swiper/swiper-bundle.min.css";
 // imports
-import './_imports';
+import "./_imports";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const resize = () => {
-    document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`)
-  }
-  resize()
+    document.documentElement.style.setProperty("--vh", `${window.innerHeight}px`);
+  };
+  resize();
 
-  window.addEventListener('resize', resize)
+  window.addEventListener("resize", resize);
 
-  function toggleClassOpen(openBtn, closeBtn, ...elems){
-    if(openBtn == closeBtn){
-      openBtn.addEventListener('click', () => {
-        elems.forEach(elem => {
-          elem.classList.toggle('open')
-        })
-      })
-    }
-    else{
-      if(openBtn){
-        openBtn.addEventListener('click', () => {
-          elems.forEach(elem => {
-            elem.classList.add('open')
-          })
-        })
+  function toggleClassOpen(openBtn, closeBtn, ...elems) {
+    if (openBtn == closeBtn) {
+      openBtn.addEventListener("click", () => {
+        elems.forEach((elem) => {
+          elem.classList.toggle("open");
+        });
+      });
+    } else {
+      if (openBtn) {
+        openBtn.addEventListener("click", () => {
+          elems.forEach((elem) => {
+            elem.classList.add("open");
+          });
+        });
       }
-  
-      if(closeBtn){
-        closeBtn.addEventListener('click', () => {
-          elems.forEach(elem => {
-            elem.classList.remove('open')
-          })
-        })
+
+      if (closeBtn) {
+        closeBtn.addEventListener("click", () => {
+          elems.forEach((elem) => {
+            elem.classList.remove("open");
+          });
+        });
       }
     }
   }
 
-  const popupBtns = document.querySelectorAll('.popup-btn')
-  popupBtns.forEach(btn => {
-    const dataPopup = btn.dataset.popupOpen
-    const popup = document.querySelector(`[data-popup=${dataPopup}]`)
-    const closePopupBtn = document.querySelector(`[data-popup-close = ${dataPopup}]`)
-    toggleClassOpen(btn, closePopupBtn, popup, btn)
-  })
-})
+  const popupBtns = document.querySelectorAll(".popup-btn");
+  popupBtns.forEach((btn) => {
+    const dataPopup = btn.dataset.popupOpen;
+    const popup = document.querySelector(`[data-popup=${dataPopup}]`);
+    const closePopupBtn = document.querySelector(`[data-popup-close = ${dataPopup}]`);
+    toggleClassOpen(btn, closePopupBtn, popup, btn);
+  });
+});
